bf3884f58b86785a3437017da2a9fa25a8f32b49
Kjellemann1 <kjellarneulvan@gmail.com>
2023-02-10 11:04:26 +0100

    changed stats::mean() to base::mean() in functions because I was right and chatGPT was wrong >:C

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,5 +1,5 @@
 bootstrap <- function(B, x, replacement = TRUE) {
 	boot <- rep(0, B)
-	for (i in 1:B) {boot[i] <- stats::mean(x[base::sample(1:base::NROW(x), replace = replacement)])}
+	for (i in 1:B) {boot[i] <- base::mean(x[base::sample(1:base::NROW(x), replace = replacement)])}
 	return(boot)
 }
