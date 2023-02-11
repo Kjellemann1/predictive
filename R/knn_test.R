baefaa06973ab55331c638ac888a5a165f74bd98
Kjellemann1 <kjellarneulvan@gmail.com>
2023-02-11 21:05:19 +0100

    test_knn() added, README updated

---
+++ R/knn_test.R
@@ -0,0 +1,25 @@
+#' knn_test
+#'
+#' @description calculates predictions for each value in test data
+#'
+#' @param test_x0 test data column
+#' @param train_x train data column
+#' @param train_y train data column with values you are trying to predict
+#' @param k k nearest neighbours to consider
+#'
+#' @examples
+#' Using the palmerpenguins dataset
+#'
+#' predictions <- knn_test(test$flipper_length_mm, train$flipper_length_mm, train$species, 3)
+#'
+#'@return vector with predictions
+#'
+#' @export
+#' @name knn_test
+knn_test <- function(test_x0, train_x, train_y, k) {
+	predictions <- rep(0, NROW(test))
+	for (i in 1:NROW(test)) {
+		predictions[i] <- knn(test_x0[i], train_x, train_y, k)
+	}
+	return(predictions)
+}
