484615dc0dd860e53519dcb96ef03dee6e10975b
Kjellemann1 <kjellarneulvan@gmail.com>
2023-02-12 14:37:46 +0100

    confusion_matrix() documentation update

--- R/confusion_matrix.R
+++ R/confusion_matrix.R
@@ -1,20 +1,20 @@
 #' confusion_matrix
-#' 
-#' @description General function that returns a confusion matrix
-#' 
+#'
+#' @description Simplifies making a confusion matrix
+#'
 #' @param test_y column from test dataframe you are trying to predict
 #' @param predictions vector containing predictions
 #' @param margin (default : 1) parameter in the prop.table() function from base R
-#' 
+#'
 #' @return Returns a table in the form of a confusion matrix
-#' 
+#'
 #' @examples
 #' Predicting default in the Default dataset from the ISLR2 package
-#' 
+#'
 #' regression <- glm(default ~ income + balance, data = train, family = 'binomial')
 #' predictions <- predict(regression, newdata = test, type = 'response') > 0.5)
 #' confusion_matrix(test, 'default', predictions)
-#' 
+#'
 #' @export
 #' @name confusion_matrix
 confusion_matrix <- function(test_y, predictions, margin = 1) {
