99ca9bf2e98aeaee9159b618288a5da5782dd05b
Kjellemann1 <kjellarneulvan@gmail.com>
2023-02-12 09:22:26 +0100

    split_train_test() no longer has default fraction argument

--- R/split_train_test.R
+++ R/split_train_test.R
@@ -1,9 +1,9 @@
 #' split_train_test
 #'
-#' @description Split dataframe into training and test data
+#' @description Split dataframe into train and test data
 #'
 #' @param df dataframe you want to split
-#' @param train_frac (default: 0.7) the fraction of the data you want in training data
+#' @param train_frac the fraction of the data you want in train
 #' @param keep_id (default: TRUE) whether or not to keep split_train_test_id column in returned dataframes
 #'
 #' @return list containing two dataframes: train and test
@@ -11,13 +11,14 @@
 #' @examples
 #' Splitting the dataframe auto, with 80% in train and 20% in test
 #'
-#' split <- split_train_test(auto, train_frac = 0.8)
+#' split <- split_train_test(auto, 0.8)
 #' train <- split$train
 #' test <- split$test
 #'
 #' @export
+#' @import dplyr
 #' @name split_train_test
-split_train_test <- function(df, train_frac = 0.7, keep_id = TRUE) {
+split_train_test <- function(df, train_frac, keep_id = TRUE) {
 	df$split_train_test_id = 1:base::NROW(df)
 	train <- df %>% dplyr::sample_frac(train_frac)
 	test <- dplyr::anti_join(df, train, by = 'split_train_test_id')
