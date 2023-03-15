7e7ba356a07d17539e1a6c03b3501321106d7dea
Kjellemann1 <kjellarneulvan@gmail.com>
2023-03-15 09:45:31 +0100

    cpp function now defined inside bootstrap.R

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,3 +1,24 @@
+library(Rcpp)
+cppFunction('
+Rcpp::NumericVector cpp_bootstrap(int B, Rcpp::NumericVector x, double quantile = -1, bool replacement = true) {
+    Rcpp::NumericVector boot(B);
+    int n = x.size();
+    if (quantile == -1) {
+        for (int i = 0; i < B; ++i) {
+            Rcpp::NumericVector sample = Rcpp::sample(x, n, replacement);
+            boot(i) = Rcpp::mean(sample);
+        }
+    } else {
+        for (int i = 0; i < B; ++i) {
+            Rcpp::NumericVector sample = Rcpp::sample(x, n, replacement);
+            std::sort(sample.begin(), sample.end());
+            int index = static_cast<int>(std::floor(quantile * n));
+            boot(i) = sample(index);
+        }
+    }
+    return boot;
+}
+')
 #' bootstrap
 #'
 #' @description Resampling using the bootstrap method
@@ -21,10 +42,8 @@
 #' sampling_distribution_99th <- bootstrap(1000, Auto$mpg, 0.99)
 #'
 #' @import Rcpp
-#' @useDynLib predictive
 #' @name bootstrap
 #' @export
 bootstrap <- function(B, x, quantile = -1, replacement = TRUE) {
-	.Call('cpp_bootstrap', B, x, quantile, replacement)
+	cpp_bootstrap(B, x, quantile, replacement)
 }
-
